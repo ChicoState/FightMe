3404dc77b1d8244c47ac5f4c98bafa2b064d4492
Taiga O'Donnell <taigaodonnell2001@gmail.com>
2024-09-23 15:46:17 -0700

    Changed filename

---
+++ backend/src/main/java/com/backend/fightmebackend/FightmebackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.fightmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightmebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightmebackendApplication.class, args);
+	}
+
+}
