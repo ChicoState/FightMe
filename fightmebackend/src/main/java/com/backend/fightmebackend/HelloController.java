aa67dd50ba7e216a0169b522883f977254584fd2
Taiga O'Donnell <taigaodonnell2001@gmail.com>
2024-09-23 13:05:48 -0700

    implemented simple hello world

---
+++ fightmebackend/src/main/java/com/backend/fightmebackend/HelloController.java
@@ -0,0 +1,17 @@
+package com.backend.fightmebackend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+    
+    
+}
