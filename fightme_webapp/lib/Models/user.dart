5fd0ffb00e51dea3fdb7dea4957e75249cc6f048
enlegato <enlegato@csuchico.edu>
2024-10-04 16:25:38 -0700

    moved user class to its own dart file

---
+++ fightme_webapp/lib/Models/user.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
+import 'package:flutter/material.dart';
+
+
+class User{
+  int id = 0;
+  String name = "";
+  String email = "";
+  String password = "";
+  int dateCreated = 0;
+  int gamerScore = 0;
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    password = json['password'];
+    dateCreated = json['dateCreated'];
+    gamerScore = json['gamerScore'];
+  }
+
+  Map<String, dynamic> toJson() =>{
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'dateCreated': dateCreated,
+      'gamerScore': gamerScore
+  };
+  
+  User(String n){
+    id = 0;
+    name = n;
+    email = "test";
+    password = "test";
+    dateCreated = 0;
+    gamerScore = 0;
+  }
+}
