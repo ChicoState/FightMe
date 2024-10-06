03c04d590f272495a294fd58f3e8682d071464f1
Sean Minami <sminami3@csuchico.edu>
2024-10-06 15:54:26 -0700

    Add call to get messages by chatroomID

--- fightme_webapp/lib/main.dart
+++ fightme_webapp/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart';
-
+import 'chat_page.dart';
+import 'Models/chatroom.dart';
+import 'Models/message.dart';
 import 'Models/user.dart';
 
 import 'package:flutter/material.dart';
@@ -10,10 +12,13 @@ void main() {
 }
 
 class HttpService {
-  final String springbootURL = "http://localhost:8080/api/users";
+  final String springbootUserURL = "http://localhost:8080/api/users/";
+  final String springbootChatroomURL = "http://localhost:8080/api/chatroom/";
+  final String springbootMessageURL = "http://localhost:8080/api/messages/";
+
   //retrieve a list of users from springboot
   Future<List<User>> getUsers() async {
-    Response res = await get(Uri.parse(springbootURL));
+    Response res = await get(Uri.parse(springbootUserURL));
     print(res.body);
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
@@ -26,7 +31,7 @@ class HttpService {
   }
 
   void postUser(User user) async {
-    Response res = await post(Uri.parse(springbootURL),
+    Response res = await post(Uri.parse(springbootUserURL),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode(user.toJson()));
     print(res.body);
@@ -34,6 +39,18 @@ class HttpService {
       print("User created successfully.");
     } else {
       throw "Unable to create user.";
+    }
+  }
+
+  Future<List<Message>> getChatroomMessages(int chatroomID) async {
+    Response res = await get(Uri.parse("$springbootMessageURL$chatroomID"));
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<Message> messages =
+          body.map((dynamic item) => Message.fromJson(item)).toList();
+      return messages;
+    } else {
+      throw "Unable to retrieve message data for chatroom $chatroomID";
     }
   }
 }
