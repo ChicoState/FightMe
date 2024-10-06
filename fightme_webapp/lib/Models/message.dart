cab31bb908852b221c84d050ad43f36643100a09
Sean Minami <sminami3@csuchico.edu>
2024-10-06 15:32:00 -0700

    Add missing parameter

--- fightme_webapp/lib/Models/message.dart
+++ fightme_webapp/lib/Models/message.dart
@@ -1,6 +1,7 @@
 class Message {
   int fromId = 0;
   int toId = 0;
+  int chatroomId = 0;
   String content = "";
   bool isRead = false;
   DateTime timeStamp = DateTime(0);
@@ -11,6 +12,7 @@ class Message {
     content = json['content'];
     isRead = json['isRead'];
     timeStamp = json['timeStamp'];
+    chatroomId = json['chatroomId'];
   }
 
   Map<String, dynamic> toJson() => {
@@ -18,14 +20,16 @@ class Message {
         'toId': toId,
         'content': content,
         'isRead': isRead,
-        'timeStamp': timeStamp
+        'timeStamp': timeStamp,
+        'chatroomId': chatroomId
       };
 
-  Message(int to, int from, String text) {
+  Message(int to, int from, String text, int chatId) {
     toId = to;
     fromId = from;
     content = text;
     isRead = false;
     timeStamp = DateTime.now();
+    chatroomId = chatId;
   }
 }
