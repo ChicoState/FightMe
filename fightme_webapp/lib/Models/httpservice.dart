3d440db715e547365568e0991915f80c2c4f6036
Taiga O'Donnell <taigaodonnell2001@gmail.com>
2024-10-16 12:42:06 -0700

    Friend requests should be able to work with frontend now

--- fightme_webapp/lib/Models/httpservice.dart
+++ fightme_webapp/lib/Models/httpservice.dart
@@ -4,11 +4,13 @@ import 'package:http/http.dart';
 
 import 'user.dart';
 import 'message.dart';
+import 'friend_request.dart';
 
 class HttpService {
   final String springbootUserURL = "http://localhost:8080/api/users/";
   final String springbootChatroomURL = "http://localhost:8080/api/chatroom/";
   final String springbootMessageURL = "http://localhost:8080/api/messages";
+  final String springbootFriendRequestURL = "http://localhost:8080/api/friendrequests";
 
   //retrieve a list of users from springboot
   Future<List<User>> getUsers() async {
@@ -68,6 +70,55 @@ class HttpService {
     } else {
       print("Status Code: ${res.statusCode}");
       throw "Unable to post message to chatroom ${message.chatroomId}";
+    }
+  }
+
+  //send a friend request to a user
+  Future<void> sendFriendRequest(int fromUserID, int toUserID) async {
+    Response res = await post(Uri.parse("$springbootFriendRequestURL$fromUserID"),
+      headers: {"Content-Type": "application/json"}, 
+      body: jsonEncode({"fromUserID": fromUserID, "toUserID": toUserID}));
+    if (res.statusCode == 201) {
+      print("Friend request sent successfully.");
+    } else {
+      throw "Unable to send friend request.";
+    }
+  }
+
+  //get all friend requests from a user
+  Future<List<FriendRequest>> getAllFriendRequests(int userID) async {
+    Response res = await get(Uri.parse("$springbootFriendRequestURL/$userID"));
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<FriendRequest> friendRequests =
+          body.map((dynamic item) => FriendRequest.fromJson(item)).toList();
+      return friendRequests;
+    } else {
+      throw "Unable to retrieve friend request data for user $userID";
+    }
+  }
+
+  //accept a friend request from a user
+  Future<void> acceptFriendRequest(int friendRequestID) async {
+    Response res = await put(Uri.parse("$springbootFriendRequestURL/accept"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"id": friendRequestID}));
+    if (res.statusCode == 200) {
+      print("Friend request accepted successfully.");
+    } else {
+      throw "Unable to accept friend request.";
+    }
+  }
+
+  // reject a friend request from a user
+  Future<void> rejectFriendRequest(int friendRequestID) async {
+    Response res = await put(Uri.parse("$springbootFriendRequestURL/reject"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"id": friendRequestID}));
+    if (res.statusCode == 200) {
+      print("Friend request rejected successfully.");
+    } else {
+      throw "Unable to reject friend request.";
     }
   }
 }
