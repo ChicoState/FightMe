8d06c18e2c56c822e839c94276fec7df8387b89a
Taiga O'Donnell <taigaodonnell2001@gmail.com>
2024-10-27 15:04:55 -0700

    Changed the response of the authentication login/signup and updated documentation on README.md

--- backend/src/main/java/com/backend/backend/auth/AuthController.java
+++ backend/src/main/java/com/backend/backend/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.backend.backend.auth;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backend.backend.ResourceNotFoundException;
 import com.backend.backend.user.User;
 import com.backend.backend.user.UserMapper;
 import com.backend.backend.user.UserRepository;
 import com.backend.backend.user.Dto.UserDto;
-
+import com.backend.backend.user.UserMapper;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -24,7 +26,7 @@ public class AuthController {
 
     // Registers a new user, and creates it in the database
     @PostMapping("signup")
-    public ResponseEntity<?> registerUser(@RequestBody UserDto userDto){
+    public ResponseEntity<?> registerUser(@RequestBody UserDto userDto){                                 //Can be refactored
         if (userDto.getPassword() == null || userDto.getPassword().length() < 8) {                      //Checks if the password is there and is at least 8 characters long 
             return ResponseEntity.badRequest().body("Password must be at least 8 characters long");
         }
@@ -37,13 +39,15 @@ public class AuthController {
             return ResponseEntity.badRequest().body("Name cannot be empty");                        //Checks if the name is there
         }
         userRepository.save(user);                                                                        //Saves the user to the database  
-        return ResponseEntity.ok("User registered");
+        User savedUser = userRepository.findByEmail(userDto.getEmail()).orElseThrow(() -> new ResourceNotFoundException("User not found: " + userDto.getEmail()));
+        long savedId = savedUser.getId();
+        return new ResponseEntity<>(savedId, HttpStatus.CREATED);                                         //Returns the user id
     }
 
 
     // Logs in the user
     @PostMapping("login")
-    public ResponseEntity<?> loginUser(@RequestBody UserDto userDto){       
+    public ResponseEntity<?> loginUser(@RequestBody UserDto userDto){               //Can be refactored
         User user = UserMapper.mapToUser(userDto);
         if(!authRepository.findByEmail(user.getEmail())){                           //Checks if the email exists
             return ResponseEntity.badRequest().body("Email does not exist");
@@ -51,6 +55,8 @@ public class AuthController {
         if(!authRepository.authenticate(user.getEmail(), user.getPassword())){      //Checks if the password is correct
             return ResponseEntity.badRequest().body("Password does not match");
         }
-        return ResponseEntity.ok("Successfully logged in");
+        User savedUser = userRepository.findByEmail(userDto.getEmail()).orElseThrow(() -> new ResourceNotFoundException("User not found: " + userDto.getEmail()));
+        long savedId = savedUser.getId();
+        return new ResponseEntity<>(savedId, HttpStatus.OK);      //Returns the user id
     }
 }
