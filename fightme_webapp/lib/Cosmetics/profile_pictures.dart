da71c79707a5e8c2a8004fd90f9c9d456a4b8efc
nrakocevic <nrakocevic@csuchico.edu>
2024-12-01 17:46:41 -0800

    Fleshed out the store page so the user can buy profile pictures and themes.

--- fightme_webapp/lib/Cosmetics/profile_pictures.dart
+++ fightme_webapp/lib/Cosmetics/profile_pictures.dart
@@ -2,4 +2,12 @@
 
 List<String> profilePictures = [
   "assets/images/dummyKnight.png",
+  "assets/images/knight-green.png",
+  "assets/images/fknight-green.png",
+];
+
+// Structure: [element in profilePictures, price]
+List<List<int>> buyableProfilePictures = [
+  [1, 4000],
+  [2, 8000],
 ];
